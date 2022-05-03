62fafd9c8240daf0c3f3ac5a30104961807ac4d3
victor del <victor.delvalle@thecolvinco.com>
2022-05-03 18:27:45 +0200

    remove unnecesary

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,9 +1,7 @@
 import CommandConsumer from './CommandConsumer';
 import DomainEventConsumer from './DomainEventConsumer';
-import MessageDeleter from '../commands/MessageDeleter';
 
 export {
   CommandConsumer,
   DomainEventConsumer,
-  MessageDeleter,
 };
